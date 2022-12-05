a651717a36c8da00d5c87f17ad58e9d4f0e0bc00
Jan Riedel <git@rjan.dev>
2022-12-05 07:05:23 +0100

    solve day 5 part 1

---
+++ src/main/kotlin/dev/rjan/aoc2022/day5/SupplyStacks.kt
@@ -0,0 +1,72 @@
+package dev.rjan.aoc2022.day5
+
+import dev.rjan.aoc2022.readInputFile
+
+fun main() {
+    print(part1())
+    println()
+    print(part2())
+}
+
+fun part1(): String {
+    val input = readInputFile("day5").split(System.lineSeparator())
+    var stacks = mutableListOf<String>()
+    for (line in input) {
+        if ("" == line) {
+            break
+        }
+        stacks.add(line)
+    }
+    var columns = mutableMapOf<Int, MutableList<String>>()
+    var columnNames = stacks[stacks.size - 1]
+    var last = columnNames[columnNames.length - 1].digitToInt()
+    for (n in 1..last) {
+        columns[n] = mutableListOf()
+    }
+    for (stack in stacks) {
+        for (n in 1..last) {
+
+            val offset = (n - 1) * 4
+            if (stack.length > offset && stack[offset] == '[') {
+                columns[n]?.add(stack[offset + 1].toString())
+            }
+        }
+
+//        if (stack[0] == '[') {
+//            columns[1]?.add(stack[1].toString())
+//        }
+//        if (stack.length > 4 && stack[4] == '[') {
+//            columns[2]?.add(stack[5].toString())
+//        }
+//        if (stack.length > 8 && stack[8] == '[') {
+//            columns[3]?.add(stack[9].toString())
+//        }
+    }
+    for (line in input) {
+        if (!line.startsWith("move")) {
+            continue
+        }
+        val destructured = "move\\s([0-9]*)\\sfrom\\s([0-9]*)\\sto ([0-9]*)".toRegex().find(line)?.destructured
+        val count = destructured?.component1()?.toLong()!!
+        val from = destructured?.component2()?.toInt()!!
+        val to = destructured?.component3()?.toInt()!!
+
+        for (n in 1..count) {
+            val removeAt = columns[from]?.removeAt(0)
+            columns[to]?.add(0, removeAt!!)
+        }
+
+
+    }
+
+    var result = ""
+    for (n in 1..last) {
+        result += columns[n]?.get(0) ?: ""
+    }
+
+    return result
+}
+
+fun part2(): String {
+    return ""
+}
---
+++ src/test/kotlin/dev/rjan/aoc2022/day5/SupplyStacksKtTest.kt
@@ -0,0 +1,16 @@
+package dev.rjan.aoc2022.day5
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SupplyStacksKtTest {
+
+    @Test
+    fun day5part1() {
+        assertEquals("CMZ", part1())
+    }
+
+    @Test
+    fun day5part2() {
+    }
+}
