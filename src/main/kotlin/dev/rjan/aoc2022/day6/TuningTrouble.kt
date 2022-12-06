c0080a3cf0574044d5ba19a0448d397cb2391768
Jan Riedel <git@rjan.dev>
2022-12-06 06:56:11 +0100

    refactor day 6

--- src/main/kotlin/dev/rjan/aoc2022/day6/TuningTrouble.kt
+++ src/main/kotlin/dev/rjan/aoc2022/day6/TuningTrouble.kt
@@ -11,28 +11,19 @@ fun main() {
 
 fun part1(): Int {
     val input = readInputFile("day6")
-    for ((index, first) in input.withIndex()) {
-        val second = input[index + 1]
-        val third = input[index + 2]
-        val fourth = input[index + 3]
-        if (hashSetOf(first, second, third, fourth).size == 4) {
-            return index + 4
-        }
-    }
-    return 0;
+    return findMarker(input, 4)
 }
 
 
 fun part2(): Int {
     val input = readInputFile("day6")
-    for ((index, _) in input.withIndex()) {
-        val set = hashSetOf<Char>()
-        for (n in 0..13) {
-            set.add(input[index + n])
-        }
-        if (set.size == 14) {
-            return index + 14
-        }
-    }
-    return 0;
+    return findMarker(input, 14)
+}
+
+private fun findMarker(input: String, markerLength: Int): Int {
+    val windowed = input.windowed(markerLength)
+    return windowed.stream()
+        .filter { it.toSet().size == markerLength }
+        .map { windowed.indexOf(it) }
+        .map { it + markerLength }.findFirst().orElse(-1)
 }
