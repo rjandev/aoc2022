c60b3424884fac534ef979f3f645f6344b4dcd0a
Jan Riedel <git@rjan.dev>
2022-12-20 19:48:09 +0100

    solve day 13 part 1

---
+++ src/test/kotlin/dev/rjan/aoc2022/day13/DistressSignalKtTest.kt
@@ -0,0 +1,16 @@
+package dev.rjan.aoc2022.day13
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class DistressSignalKtTest {
+
+    @Test
+    fun day13part1() {
+        assertEquals(13, part1())
+    }
+
+    @Test
+    fun day13part2() {
+    }
+}
