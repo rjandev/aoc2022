b0bbbaad98fc0a7c3c2754c7a1b69b4987264615
Jan Riedel <git@rjan.dev>
2022-12-26 07:11:02 +0100

    solve day 25

---
+++ src/test/kotlin/dev/rjan/aoc2022/day25/FullOfHotAirKtTest.kt
@@ -0,0 +1,16 @@
+package dev.rjan.aoc2022.day25
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class FullOfHotAirKtTest {
+
+    @Test
+    fun day25part2() {
+    }
+
+    @Test
+    fun day25part1() {
+        assertEquals("2=-1=0", part1())
+    }
+}
